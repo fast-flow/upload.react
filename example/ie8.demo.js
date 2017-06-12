5d767de8f6ef3e4f5c045248ee6194b6a56b5479
Grifree <1149290648@qq.com>
2017-06-12 10:09:00 +0800

    重置ie实现逻辑

--- example/ie8.demo.js
+++ example/ie8.demo.js
@@ -16,32 +16,13 @@ var App = React.createClass({
                 <UploadPicker name="file" action="/upload?status=success"
                     data={{'a':'1'}}
                     thumb={'http://dummyimage.com/200x200/000/fff?text=thumb'}
-                    // onPick={function (files) {
-                    //     console.log(files)
-                    //     self.setState({
-                    //         src:files[0].thumb
-                    //     })
+                    onPick={function (files) {
+                        console.log(files)
+                        self.setState({
+                            src:files[0].thumb
+                        })
 
-                    // }}
-                    onStart={function(file) {
-					    console.log('onStart', file, file.name);
-					}}
-					onSuccess={function(ret) {
-					    console.log('onSuccess', ret);
-					}}
-					onError={function(err) {
-					    console.log('onError', err);
-					}}
-					beforeUpload={function(file, fileList) {
-					    console.log(file, fileList);
-					    return new Promise((resolve) => {
-						    console.log('start check');
-						    setTimeout(() => {
-						        console.log('check finshed');
-						        resolve(file);
-						    }, 3000);
-					    });
-					}}
+                    }}
                  >
                     <button type="button">Picker</button>
                 </UploadPicker>
