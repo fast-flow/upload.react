b41673b338c0c196e6864741cf3947b1ef0b8bad
Grifree <1149290648@qq.com>
2017-06-12 23:53:20 +0800

    IE初步逻辑架构完成[1]TODO标识待完善[2]Iframe css 待完善

--- example/ie8.demo.js
+++ example/ie8.demo.js
@@ -1,5 +1,6 @@
 var React = require('react')
 var UploadPicker = require('upload.react').default
+var UploadFile = require('upload.react').UploadFile
 
 
 var App = React.createClass({
@@ -13,15 +14,51 @@ var App = React.createClass({
         var self = this
         return (
             <div>
+            	<h3>一次只能选择一个文件</h3>
+	            {/* TODO : allow , size , onError */}
                 <UploadPicker name="file" action="/upload?status=success"
                     data={{'a':'1'}}
                     thumb={'http://dummyimage.com/200x200/000/fff?text=thumb'}
                     onPick={function (files) {
-                        console.log(files)
+                        alert('onPick : '+files[0].id)
+                        /* ie下files.length == 0
+                        files: [
+                            {
+                                id: '3fiuhwufhweufgwef',
+                                name: 'ashdasdasd.jpg',
+                                // thumb base64 or defaultThumb
+                                // defaultThumb blob
+                                thumb: 'BASE64:sufihsiufh'
+                            },
+                        ]
+                        */
                         self.setState({
                             src:files[0].thumb
                         })
 
+						UploadFile({
+                            id : files[0].id ,
+                            onProgress : function (step, file) {
+                                /*
+                                    // 精确的浮点数
+                                    step = 30.214124
+                                */
+                                // let percent = Math.round(step.percent)
+                                console.info('上传进度', step.percent)
+                            },
+                            onSuccess : function (res) {
+                            	alert('onSuccess : '+JSON.stringify(res))
+                                self.setState({
+                                    id: res.data.id
+                                })
+                            },
+                            onError : function (err, res){
+                                console.log(err,res)
+                            },
+                            onXhrError: function(e) {
+                                console.log(e)
+                            }
+                        })
                     }}
                  >
                     <button type="button">Picker</button>
